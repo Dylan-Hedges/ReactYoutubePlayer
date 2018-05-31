98e114a5637f0c4fad173ea5716ad7ae2accc605
DylanHedges <d.hedges1@hotmail.co.uk>
2018-05-31 07:20:31 -0400

    Added recommended video lists

--- src/App.js
+++ src/App.js
@@ -16,21 +16,27 @@ const API_KEY = `${keys.youtubeKey}`;
 class App extends Component {
      constructor(props){
          super(props)
-         //Sets inital state - an object containing an empty Array and selected video to nothing
+         //Sets inital app state
          this.state = {
              videos1: [],
              videos2: [],
              videos3: [],
+             videos4: [],
+             videos5: [],
+             videos6: [],
+             videos7: [],
+             videos8: [],
+             videos9: [],
+             videos10: [],
              selectedVideo: null
          };
-
          //Inital search terms on page load
-         this.videoSearch({search1: 'javascript', search2: 'cars', search3:'camping'});
+         this.videoSearch({search1: 'javascript', search2: 'schmoyoho', search3:'college humor originals', search4: 'phillip de franco', search5: 'fred', search6: 'ray william johnson', search7: 'epic meal time', search8: 'fps russia', search9: 'gangnam style', search10: 'machinima'});
      }
 
-     //Executes YouTube Searches
+     //Function that executes YouTube Searches
      videoSearch(terms){
-         //Searches for YouTube Videos
+         //Makes request to YouTube API
          YTSearch({key: API_KEY, term: terms.search1}, (videos) => {
              this.setState({
                  videos1: videos,
@@ -47,32 +53,112 @@ class App extends Component {
                  videos3: videos
              });
          });
+         YTSearch({key: API_KEY, term: terms.search4}, (videos) => {
+             this.setState({
+                 videos4: videos
+             });
+         });
+         YTSearch({key: API_KEY, term: terms.search5}, (videos) => {
+             this.setState({
+                 videos5: videos
+             });
+         });
+         YTSearch({key: API_KEY, term: terms.search6}, (videos) => {
+             this.setState({
+                 videos6: videos
+             });
+         });
+         YTSearch({key: API_KEY, term: terms.search7}, (videos) => {
+             this.setState({
+                 videos7: videos
+             });
+         });
+         YTSearch({key: API_KEY, term: terms.search8}, (videos) => {
+             this.setState({
+                 videos8: videos
+             });
+         });
+         YTSearch({key: API_KEY, term: terms.search9}, (videos) => {
+             this.setState({
+                 videos9: videos
+             });
+         });
+         YTSearch({key: API_KEY, term: terms.search10}, (videos) => {
+             this.setState({
+                 videos10: videos
+             });
+         });
      }
 
      //Renders components to the page
      render(){
          //Slows down/throttles user searches (makes results return smoother) - Creates a new debounched version of the search function that can be called only once every 300 milliseconds
          const videoSearch = _.debounce((term) => {this.videoSearch(term) },300);
+         //Returns content to screen
          return(
-            <div className="container">
-               <SearchBar onSearchTermChange={videoSearch}/>
-               <div className = {classes.mainAndSideList}>
+           <div className="container">
+              <SearchBar onSearchTermChange={videoSearch}/>
+              <div className = {classes.mainAndSideList}>
                   <VideoDetail video={this.state.selectedVideo}/>
                   <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
                     videos={this.state.videos1} sideList={true}/>
-               </div>
-               <div>
-                 <VideoList
-                 onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-                   videos={this.state.videos2} sideList={false}/>
-               </div>
-               <div>
-                 <VideoList
-                 onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+              </div>
+              <div>
+                  <h3>Schmoyoho</h3>
+                  <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos2} sideList={false}/>
+              </div>
+              <div>
+                  <h3>College Humor</h3>
+                  <VideoList
+                   onVideoSelect={selectedVideo => this.setState({selectedVideo})}
                    videos={this.state.videos3} sideList={false}/>
-               </div>
-             </div>
+              </div>
+              <div>
+                  <h3>Phillip De Franco</h3>
+                  <VideoList
+                   onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                   videos={this.state.videos4} sideList={false}/>
+              </div>
+              <div>
+                  <h3>Fred</h3>
+                  <VideoList
+                   onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                   videos={this.state.videos5} sideList={false}/>
+              </div>
+              <div>
+                  <h3>Ray William Johnson</h3>
+                  <VideoList
+                   onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                   videos={this.state.videos6} sideList={false}/>
+              </div>
+              <div>
+                  <h3>Epic Meal Time</h3>
+                  <VideoList
+                   onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                   videos={this.state.videos7} sideList={false}/>
+              </div>
+              <div>
+                  <h3>FPSRussia</h3>
+                  <VideoList
+                   onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                   videos={this.state.videos8} sideList={false}/>
+              </div>
+              <div>
+                  <h3>Gangnam Style</h3>
+                  <VideoList
+                   onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                   videos={this.state.videos9} sideList={false}/>
+              </div>
+              <div>
+                  <h3>FPSRussia</h3>
+                  <VideoList
+                   onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                   videos={this.state.videos10} sideList={false}/>
+              </div>
+            </div>
          );
      }
  }
